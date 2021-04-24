4406cbf6aeaec8188880499f6d4f1a8e842c14b4
nhoward712 <Nhoward72@yahoo.com>
2021-04-24 17:43:07 -0500

    * .gitignore: Initial
    * Module4Homework.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * Module4Homework.csproj:
    * Entitlements.plist:
    * MainActivity.cs:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * Module4Homework.iOS.csproj:
    * AboutAssets.txt:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * Module4Homework.Android.csproj:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon76.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon80.png:
    * icon_round.xml:
    * Icon120.png:
    * Icon152.png:
    * Icon167.png:
    * Icon180.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ Module4Homework/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module4Homework
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+
+        }
+        void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+
+        }
+        void OnTextCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+    }
+}
